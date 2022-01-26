e459129012e457ee0de0fdc3ecc9d367227242fb
Septiana Yoga <septianayoga30@gmail.com>
2022-01-26 14:50:06 +0700

    fixing minor error

--- src/components/userDetails.js
+++ src/components/userDetails.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 
 export default class UserDetails extends Component {
   render() {
+    // console.log(this.props.biodesc.length);
+    const biodesc =
+      this.props.biodesc.length > 220
+        ? this.props.biodesc.substring(0, 220) + "..."
+        : this.props.biodesc;
     return (
       <div className="user-details">
         <div className="name-container">
           <h3 className="username">{this.props.name}</h3>
           <span className="nametag">@{this.props.nametag}</span>
         </div>
-        <p className="bio-desc">{this.props.biodesc}</p>
+        <p className="bio-desc">{biodesc}</p>
         <div className="follower-container">
           <p className="followers">
             1.3k <span className="followers-text">Followers</span>
